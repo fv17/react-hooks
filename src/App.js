b8f0e45d1140712c27ec9783903c0f94716d5e5e
fv17 <forest.valley0417@gmail.com>
2019-08-06 10:18:46 +0900

    study: effect hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [count, setCount] = useState(0);
   const [fruit, setFruit] = useState('Banana');
+
+
+  // クリーンアップを必要としない副作用
+  // 例えば、ネットワークリクエストの送信、手動での DOM 改変、ログの記録など
+
+  // クリーンアップを有する副作用
+  // 例えば、外部のデータソースへの購読など
+  // メモリリークを防ぐためにクリーンアップが必要
+
+  // DOMの更新後に渡した関数の処理が行われる
+  useEffect(() => {
+    document.title = `You Clicked ${count} times!`
+
+    // クリーンアップが必要な場合、ここで関数を返す
+  });
 
   const handleAddClick = () => {
     setCount(count + 1)
